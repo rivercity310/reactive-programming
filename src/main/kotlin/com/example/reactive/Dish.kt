358e9e870f1bae5a7c49b024e2cb79727e362d67
seungsu <h970126@gmail.com>
2023-06-10 17:31:19 +0900

    스트리밍 방식 웹 서비스 예시

---
+++ src/main/kotlin/com/example/reactive/Dish.kt
@@ -0,0 +1,18 @@
+package com.example.reactive
+
+class Dish(
+    var description: String,
+    var delivered: Boolean = false
+) {
+    companion object {
+        internal fun deliver(dish: Dish): Dish {
+            val deliveredDish = Dish(dish.description)
+            deliveredDish.delivered = true
+            return deliveredDish
+        }
+    }
+
+    override fun toString(): String {
+        return "Dish{description=${description}, delivered=${delivered}}"
+    }
+}
---
+++ src/main/kotlin/com/example/reactive/KitchenService.kt
@@ -0,0 +1,28 @@
+package com.example.reactive
+
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import java.time.Duration
+import kotlin.random.Random
+
+@Service
+class KitchenService {
+    private val menu: List<Dish> = listOf(
+        Dish("Sesame chicken"),
+        Dish("Lo mein noodles, plain"),
+        Dish("Sweet & sour beef")
+    )
+
+    private val picker: Random = Random.Default
+
+    /* 요리 스트림 생성 */
+    internal fun getDishes(): Flux<Dish> {
+        return Flux.generate { it.next(randomDish()) }
+            .delayElements(Duration.ofMillis(250))
+    }
+
+    /* 요리 무작위 선택 */
+    private fun randomDish(): Dish {
+        return menu[picker.nextInt(menu.size)]
+    }
+}
